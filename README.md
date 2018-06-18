6d114a641c78fe9002066d6319b66da0d9ba81e0
Jennifer Bach <jmbach1@gmail.com>
2018-06-17 22:30:52 -0700

    updated readme and added tests

--- README.md
+++ README.md
@@ -1,48 +1,31 @@
-![cf](http://i.imgur.com/7v5ASc8.png) Linked List Data Structure
-====
+# 02-Linked LIsts
 
-## To Submit this Assignment
-  * Do all your work in a public repository called `data-structures-and-algorithms`
-  * fork this repository
-  * write all of your code in a directory named `linked-lists`
-  * push to your repository
-  * submit a pull request to this repository
-  * submit a link to your PR in canvas
-  * write a question and observation on canvas
+Link to Master: https://github.com/jbach197/05-linked-lists
+Link to Pull Request: 
+Link to Travis: 
 
-## Requirements  
-#### Configuration  
-  <!-- list of files, configurations, tools, etc that are required -->
-  Your lab directory must include  
-  * **index.js** -- The entry point to your module
-  * **README.md** -- with a documentation about your lab
-  * **.gitignore** -- with a robust .gitignore
-  * **.eslintrc** -- with the class .eslintrc file
-  * **.eslintignore** -- with the class .eslintignore
-  * **.package.json** -- with all dependencies and dev-dependencies
-  * **lib/** -- directory for holding your programs helper modules
-  * **test/** -- directory for holding your programs unit and integration tests
+# Installation Instructions
+1) Clone the repo
+2) Run npm i to install the dependencies
+3) Run npm test to complete the tests
+4) Run npm lint for the linter
 
-#### Testing  
-  * write at least three test assertions for each method of the Singly Linked List Data Structure
-  * organize your tests into appropriate describe/it blocks for test output readability
-
-####  Documentation  
-  * in your README, write documentation for you data structures
-  * your documentation should includes code block useage examples
-  * provide instructions for:
-    * installing and using your data structure
-    * accessing each method
-    * running your tests
-
-#### Feature Tasks  
-* implement a Singly Linked List (SLL) data structure
+#Requirements
+* Implement a singly linked list data structure.
 * implement `append(value)`, `prepend(value)`, `reverse()`, and `remove(offset)` methods to the SLL class
 * implement `serialize()` and `deserialize()` methods on the class
-* in a comment within each function, note the it's Big-O runtime
+* Note the it's Big-O runtime
+Inert and delete have a Big-O of O(1), reverse has O(n) where n is the number of nodes in the list.
 
-#### Rubric:
-  * Tests: 2pts
-  * Passes linter: 1pts
-  * Completed Data Structure: 5pts
-  * Big-O notation: 2pt
+#Testing
+Use the `describe` and `it` (or `test`) methods to define descriptive tests and increase readablity
+Each `it` callback should aim to test a small, well defined, feature of a function - this may include:
+  * tests that ensure the list module functions correctly with error-check parameters
+  * tests that the correct errors are thrown with invalid arguments exist
+  * tests to ensure that the list module functions return the correct results when invoked with valid arguments
+
+
+#Worked with and Credits
+Nothing specific.
+
+
--- __test__/lib/linked_list.test.js
+++ __test__/lib/linked_list.test.js
@@ -2,36 +2,87 @@
 
 let LinkedList = require('../../lib/linked_list.js');
 
-describe('linked list', () => {
+describe('linked list methods', () => {
 
-    it('constructor()', () => {
-        let list = new LinkedList();
-        expect(list.head).toBeNull();
-    });
+  it('constructor() creates a null list', () => {
+    let list = new LinkedList();
+    expect(list.head).toBeNull();
+  });
 
-    it('append()', () => {
-        let list = new LinkedList ();
-        let initialValue = 'first';
-        list.append(initialValue);
-        expect(list.head.value).toEqual(initialValue);
+  it('append() adds value to end of list', () => {
+    let list = new LinkedList ();
+    let initialValue = 'first';
+    list.append(initialValue);
+    expect(list.head.value).toEqual(initialValue);
 
-        let newValue = 'second';
-        list.append(newValue);
-        expect(list.head.value).toEqual(initialValue);
+    let newValue = 'second';
+    list.append(newValue);
+    expect(list.head.value).toEqual(initialValue);
 
-        let anotherNewOne = 'third';
-        list.append(anotherNewOne);
-    });
+    let anotherNewOne = 'third';
+    list.append(anotherNewOne);
+  });
 
-    it('prepend()', () => {
-        let list = new LinkedList ();
-        let initialValue = 'first';
-        list.append(initialValue);
-        expect(list.head.value).toEqual(initialValue);
+  it('prepend() adds value to begining of list and resets head', () => {
+    let list = new LinkedList ();
+    let initialValue = 'first';
+    list.append(initialValue);
+    expect(list.head.value).toEqual(initialValue);
 
-        let newValue = 'second';
-        list.prepend(newValue);
-        expect(list.head.value).toEqual(newValue);
-    })
+    let newValue = 'second';
+    list.prepend(newValue);
+    expect(list.head.value).toEqual(newValue);
+  });
 
-})
+  it('prepend() adds value to begining of list and resets head', () => {
+    let list = new LinkedList ();
+    let initialValue = 'first';
+    list.append(initialValue);
+    expect(list.head.value).toEqual(initialValue);
+
+    let newValue = 'second';
+    list.prepend(newValue);
+    expect(list.head.value).toEqual(newValue);
+  });
+
+  it('remove() removes head', () => {
+    let list = new LinkedList ();
+    let initialValue = 'first';
+    let newValue = 'second';
+
+    list.append(initialValue);
+    list.append(newValue);
+
+    list.remove(initialValue);
+    expect(list.head.value).toEqual(newValue);
+  });
+
+  it('remove() removes item in list', () => {
+    let list = new LinkedList ();
+    let initialValue = 'first';
+    let newValue = 'second';
+    let otherValue = 'third';
+
+    list.append(initialValue);
+    list.append(newValue);
+    list.append(otherValue);
+
+    list.remove(newValue);
+    expect(list.head.value).toEqual(initialValue);
+  });
+
+  it('reverse() reverses the linked list', () => {
+    let list = new LinkedList ();
+    let initialValue = 'first';
+    let newValue = 'second';
+    let anotherOne = 'third';
+    list.append(initialValue);
+    list.append(newValue);
+    list.append(anotherOne);
+
+    list.reverse();
+    expect(list.head.value).toEqual(anotherOne);
+    console.log(list.head.value);
+  });
+
+});
